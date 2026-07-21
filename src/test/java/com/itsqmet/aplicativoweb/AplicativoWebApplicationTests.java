f5316e4579080eea96b1a2e75c2456bfcf588e2d
Matheew <arielmathew2607@gmail.com>
2026-07-21 14:37:22 -0500

    Proyecto inicial

---
+++ src/test/java/com/itsqmet/aplicativoweb/AplicativoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsqmet.aplicativoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicativoWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
