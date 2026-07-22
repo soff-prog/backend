1d3cf0fe2c45254a00f60906747c7fea97fab186
Matheew <arielmathew2607@gmail.com>
2026-07-22 17:09:44 -0500

    Entidades Alumno, Materia y Nota con validaciones

--- src/main/java/com/itsqmet/aplicativoweb/model/Materia.java
+++ src/main/java/com/itsqmet/aplicativoweb/model/Materia.java
@@ -1,6 +1,8 @@
 package com.itsqmet.aplicativoweb.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.List;
@@ -10,13 +12,25 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 
+
+/**
+ * Entidad que representa a una materia o asignatura.
+ * Incluye nombre, descripcion y su relación con las notas
+ * que los alumnos obtienen en esta materia
+ */
+
 public class Materia {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long id;
 
+    @NotBlank(message = "El nombre de la materia no puede estar vacío")
+    @Size(min = 2, max = 50, message = "El nombre de la materia debe tener entre 2 y 50 carácteres")
     private String nombre;
+
+    @NotBlank(message = "La descripción no puede estar vacía")
+    @Size(min = 5, max = 200, message = "La descripcion debe tener entre 5 y 200 carácteres")
     private String descripcion;
 
     @OneToMany(mappedBy = "materia")
