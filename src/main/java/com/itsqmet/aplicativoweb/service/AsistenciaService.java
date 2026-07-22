298836604e0ebc30f4ad273171dcf0a8a07400c5
joseanchapaxi15-cloud <joseanchapaxi15@gmail.com>
2026-07-21 22:55:06 -0500

    creacion modelos y correccion repositorios.

--- src/main/java/com/itsqmet/aplicativoweb/service/AsistenciaService.java
+++ src/main/java/com/itsqmet/aplicativoweb/service/AsistenciaService.java
@@ -10,31 +10,36 @@ import java.util.Optional;
 public class AsistenciaService {
 
      @Autowired
-    private AsistenciaRepository asistenciaRepository;
+
+     private AsistenciaRepository asistenciaRepository;
+
      public List<Asistencia> obtenerTodas(){
          return asistenciaRepository.findAll();
     }
+
     //Buscar por id
     public Optional <Asistencia> obtenerPorId(Long id){
      return asistenciaRepository.findById(id);
     }
+
      //Registrar asistencia
     public Asistencia guardarAsistencia(Asistencia asistencia ){
          return asistenciaRepository.save(asistencia);
     }
+
     //Eliminar asistencia
     public void eliminarAsistencia (Long id){
          asistenciaRepository.deleteById(id);
     }
+
     //Historial asistencia
     public List<Asistencia> obtenerPorAlumno (Long alumnoId){
      return asistenciaRepository.findByAlumnoId(alumnoId);
     }
+
     //Filtrar por estado
     public List <Asistencia> obtenerPorAlumnoYEstado(Long alumnoId, EstadoAsistencia estado){
          return asistenciaRepository.findByAlumnoIdAndEstado(alumnoId, estado);
-
-
     }
 
 }
