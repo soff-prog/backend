33252c6e24d1605bb836855b8022d9b1d1a1b4b5
kevledex <kevinledesma014@gmail.com>
2026-07-22 18:52:49 -0500

    Se agrego las consultas JPA en los repository Alumno, Materia y Nota

--- src/main/java/com/itsqmet/aplicativoweb/repository/AlumnoRepository.java
+++ src/main/java/com/itsqmet/aplicativoweb/repository/AlumnoRepository.java
@@ -4,7 +4,26 @@ import com.itsqmet.aplicativoweb.model.Alumno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AlumnoRepository extends JpaRepository<Alumno, Long> {
 
+    //Para buscar estudiantes de un curso
+    List<Alumno> findByCurso(String curso);
+
+    //Para buscar estudiantes de un curso ordenado alfabeticamente
+    List<Alumno> findByCursoOrderByApellioAsc(String curso);
+
+    //Para buscar alumnos por su apellido ignorando si está en mayúscula
+    List<Alumno> findByApellidoContainingIgnoreCase(String apellido);
+
+    //Para una busqueda donde se puede escribir el nombre o el apellido
+    List<Alumno> findByNombreContainingIgnoreCaseOrApellidoContainingIgnoreCase(String nombre, String apellido);
+
+    //Para buscar alumnos que no tengan ninguna nota registrada
+    List<Alumno> findByNotasIsEmpty();
+
+    //Para obtener los alumnos que tienen una nota destacada de mayor o igual a cierto valor
+    List<Alumno> findDistincByNotasCalificacionGreaterThanEqual(Double calificacion);
 }
--- src/main/java/com/itsqmet/aplicativoweb/repository/NotaRepository.java
+++ src/main/java/com/itsqmet/aplicativoweb/repository/NotaRepository.java
@@ -4,7 +4,25 @@ import com.itsqmet.aplicativoweb.model.Nota;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
-public interface NotaRepository extends JpaRepository <Nota, Long> {
+public interface NotaRepository extends JpaRepository<Nota, Long> {
+
+    //Para buscar todas las notas que pertenecen a un alumno en especifico
+    List<Nota> findByAlumnoId(Long alumnoId);
+
+    //Para buscar notas registradas en una fecha en especifico
+    List<Nota> findByFecha(LocalDate fecha);
+
+    //Para buscar las notas de un alumno especifico dentro de una meteria especifica
+    List<Nota> findByAlumnoIdAndMateria(Long alumnoId, Long materiaId);
+
+    //Para buscar notas mediante el apellido de un alumno
+    List<Nota> findByAlumnoApellidoContainingIgnoreCase(String apellido);
+
+    //Buscar notas ingresadas dentro de un rango de fechas
+    List<Nota> findByFechaBetween(LocalDate fechaInicio, LocalDate fechaFin);
 
 }
