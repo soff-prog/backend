f6723297df5b0bd15d163ffbc32460da6a2f14b6
fernando <Fernandoquinatoa999@gmail.com>
2026-07-21 23:03:00 -0500

    Service curso

--- src/main/java/com/itsqmet/aplicativoweb/service/CursoService.java
+++ src/main/java/com/itsqmet/aplicativoweb/service/CursoService.java
@@ -1,12 +1,30 @@
 package com.itsqmet.aplicativoweb.service;
-import com.itsqmet.aplicativoweb.model.Asistencia;
-import com.itsqmet.aplicativoweb.model.EstadoAsistencia;
-import com.itsqmet.aplicativoweb.repository.AsistenciaRepository;
+import com.itsqmet.aplicativoweb.model.Curso;
+import com.itsqmet.aplicativoweb.repository.CursoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class CursoService {
+
+    @Autowired CursoRepository cursoRepository;
+    public List<Curso> obtenerTodas() {
+        return cursoRepository.findAll();
+    }
+    public Optional<Curso> obtenerPorId(Long id) {
+        return cursoRepository.findById(id);
+    }
+    public Curso guardarCurso (Curso curso){
+        return cursoRepository.save(curso);
+    }
+    public void eliminarCurso (Long id){
+        cursoRepository.deleteById(id);
+    }
+
+
+
+
 
 }
--- src/main/java/com/itsqmet/aplicativoweb/service/DocenteService.java
+++ src/main/java/com/itsqmet/aplicativoweb/service/DocenteService.java
@@ -1,5 +1,9 @@
 package com.itsqmet.aplicativoweb.service;
 import com.itsqmet.aplicativoweb.model.Docente;
+import com.itsqmet.aplicativoweb.repository.DocenteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
 public class DocenteService {
 }
